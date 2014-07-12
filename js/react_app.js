aee9915f6960b357d0d6581c8f768542e992221b
Carlos <dela3499@gmail.com>
2014-07-12 17:11:02 -0500

    First pass at transition page in React. Now separating out components

--- js/react_app.js
+++ js/react_app.js
@@ -4,55 +4,33 @@ var SectionTransitionPage = React.createClass({
         render: function () {
             return (
                 <div id="transition-finder">
-                    <Navigation/>
-                    <Directions/>
-                    <Player/>
+                    <div className="nav">
+                        <img className="back-button" src="img/back.svg" alt="back button"/>
+                        <img className="help-button" src="img/help.svg" alt="help button"/>
+                        <img className="continue-button" src="img/continue.svg" alt="continue button"/>
+                    </div>                
+                    <div className="main-container">
+                        <div className="directions">First, separate the song into sections<br/>by finding the big transitions.</div> 
+                        <div className="player">
+                            <div className="player-background">
+                                <div className='transition-marker' ng-repeat='t in tf.transitions' ng-style="{'left': t*100+'%'}"></div>
+                                <div className="progress-bar"></div>
+                            </div> 
+                            <div className='elapsed-time'>T_e</div>
+                            <div className='remaining-time'>-T_r</div>
+                        </div>
+                        <div className="controls">
+                            <img className="play button" src="img/play.svg"/>
+                            <img className="pause button" src="img/pause.svg"/>
+                            <img className="transition button" src="img/found-transition.svg"/>
+                            <img className="back-5 button" src="img/back5.svg"/>
+                            <img className="back-x button" src="img/back-x.svg"/>
+                        </div>
+                    </div>
                 </div>
             );
         }
     });
-var Navigation = React.createClass({
-        render: function () {
-            return (
-                <div className="nav">
-                    <img className="back-button" src="img/back.svg" alt="back button"/>
-                    <img className="help-button" src="img/help.svg" alt="help button"/>
-                    <img className="continue-button" src="img/continue.svg" alt="continue button"/>
-                </div>
-            );
-        }
-    });
-var Directions = React.createClass({
-        render: function () {
-            return (
-                <div className="directions">
-                    First, separate the song into sections<br/>by finding the big transitions.
-                </div>
-            );
-        }
-    });
-var Player = React.createClass({
-        render: function () {
-            return (
-                <div className="player">
-                    <ProgressBar/>
-                </div>
-            );
-        }
-    });
-var ProgressBar = React.createClass({
-        render: function () {
-            return (
-                <div className="player-background">
-                    <div className='transition-marker'/>
-                    <div className="progress-bar"/>
-                </div>
-            );
-        }
-    });
-            
-                    
-            
             
             
 React.renderComponent(
